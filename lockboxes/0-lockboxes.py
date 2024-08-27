7e0d45438a57781e126c9b96e9bb05fc5886f853
gastonperego <gastonperego@gmail.com>
2024-08-27 13:33:38 -0300

    fix: checking for wrong numbers

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -15,15 +15,15 @@ def canUnlockAll(boxes):
     if len(boxes[0]) == 0:
         return False
     array = boxes[0]
-    for i in range(len(array) - 1):
-        if array[i] > len(boxes) - 1:
-            array.pop(i)
     array2 = []
     while not len(array) > len(boxes) - 1 and array2 != array:
         array2 = array.copy()
         for i in range(len(array2)):
             array += boxes[array2[i]]
             array = list(set(array))
+        for i in range(len(array) - 1):
+            if array[i] > len(boxes) - 1 or array[i] == 0:
+                array.pop(i)
     for i in range(len(array) - 1):
         if array[i] == 0:
             array.pop(i)
