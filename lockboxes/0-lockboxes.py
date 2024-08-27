0b83cbb72fdcaf5d5ec27f69481ef8a11882f1ea
gastonperego <gastonperego@gmail.com>
2024-08-27 13:24:39 -0300

    fix: documentation added

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+    canUnlockAll: having n number of 'blocked boxes' (a list of lists) with
+    keys inside each, it determines if all the boxes can be open taking into
+    account that the first one is always open and it can contain keys for the
+    rest of them
+"""
 
 def canUnlockAll(boxes):
+    """
+        
+    """
     if len(boxes) == 1:
         return True
     if len(boxes[0]) == 0:
